ac9a182a2dc8ed1579c48cab60365e96b5980599
mikemtzp <mikemp8226@gmail.com>
2023-08-29 16:23:42 -0600

    add base64 url to field

--- components/locations/Field.tsx
+++ components/locations/Field.tsx
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Paragraph } from '@contentful/f36-components';
 import { FieldAppSDK } from '@contentful/app-sdk';
-import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
+import { useSDK } from '@contentful/react-apps-toolkit';
+import { createClient } from 'contentful-management';
+import nextBase64 from 'next-base64';
 
 const Field = () => {
   const sdk = useSDK<FieldAppSDK>();
-  /*
-     To use the cma, inject it as follows
-     If it is not needed, you can remove the next line.
-  */
-  // const cma = useCMA();
-  // If you only want to extend Contentful's default editing experience
-  // reuse Contentful's editor components
-  // -> https://www.contentful.com/developers/docs/extensibility/field-editors/
-  return <Paragraph>Hello Entry Field Component (AppId: {sdk.ids.app})</Paragraph>;
+  const [base64, setBase64] = useState('');
+
+  const cma = createClient(
+    { apiAdapter: sdk.cmaAdapter },
+    {
+      type: 'plain',
+      defaults: {
+        environmentId: sdk.ids.environmentAlias ?? sdk.ids.environment,
+        spaceId: sdk.ids.space,
+      },
+    }
+  );
+
+  useEffect(() => {
+    sdk.window.startAutoResizer();
+
+    const image = sdk.entry.fields.image.getValue();
+
+    cma.asset
+      .get({
+        assetId: image.sys.id,
+      })
+      .then((data) => {
+        const url = data.fields.file['en-US'].url;
+        const imageUrl = `https:${url}`.trim();
+        const base64Image = nextBase64.encode(imageUrl);
+        const base64Url = `data:image/png;base64,${base64Image}`.trim();
+        setBase64(base64Url);
+      });
+  }, [sdk, cma]);
+
+  console.log(base64);
+
+  return (
+    <div>
+      <Paragraph>{base64}</Paragraph>
+    </div>
+  );
 };
 
 export default Field;
