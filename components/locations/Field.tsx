19c43745378efe840781f0d2e486ef938f181b03
mikemtzp <mikemp8226@gmail.com>
2023-08-30 09:21:13 -0600

    persist data in base64Image data field
    display a loading component while the API call is being made

--- components/locations/Field.tsx
+++ components/locations/Field.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { Paragraph } from '@contentful/f36-components';
+import React, { useEffect, useMemo, useState } from 'react';
+import { Spinner, TextInput } from '@contentful/f36-components';
 import { FieldAppSDK } from '@contentful/app-sdk';
 import { useSDK } from '@contentful/react-apps-toolkit';
 import { createClient } from 'contentful-management';
@@ -8,21 +8,32 @@ import nextBase64 from 'next-base64';
 const Field = () => {
   const sdk = useSDK<FieldAppSDK>();
   const [base64, setBase64] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
-  const cma = createClient(
-    { apiAdapter: sdk.cmaAdapter },
-    {
-      type: 'plain',
-      defaults: {
-        environmentId: sdk.ids.environmentAlias ?? sdk.ids.environment,
-        spaceId: sdk.ids.space,
-      },
-    }
+  const cma = useMemo(
+    () =>
+      createClient(
+        { apiAdapter: sdk.cmaAdapter },
+        {
+          type: 'plain',
+          defaults: {
+            environmentId: sdk.ids.environmentAlias ?? sdk.ids.environment,
+            spaceId: sdk.ids.space,
+          },
+        }
+      ),
+    [
+      sdk.cmaAdapter,
+      sdk.ids.environmentAlias,
+      sdk.ids.environment,
+      sdk.ids.space,
+    ]
   );
 
-  useEffect(() => {
-    sdk.window.startAutoResizer();
+  useEffect(() => sdk.window.startAutoResizer(), [sdk.window]);
 
+  useEffect(() => {
+    setIsLoading(true);
     const image = sdk.entry.fields.image.getValue();
 
     cma.asset
@@ -34,17 +45,17 @@ const Field = () => {
         const imageUrl = `https:${url}`.trim();
         const base64Image = nextBase64.encode(imageUrl);
         const base64Url = `data:image/png;base64,${base64Image}`.trim();
+        sdk.entry.fields.base64Image.setValue(base64Url);
         setBase64(base64Url);
+        setIsLoading(false);
       });
-  }, [sdk, cma]);
+  }, [sdk.entry.fields.base64Image, sdk.entry.fields.image, cma.asset]);
 
-  console.log(base64);
+  if (isLoading) {
+    return <Spinner />;
+  }
 
-  return (
-    <div>
-      <Paragraph>{base64}</Paragraph>
-    </div>
-  );
+  return <TextInput name='base64Image' value={base64} isReadOnly />;
 };
 
 export default Field;
